45546800d2a32ce3610e4d88d4077091e3a257b1
Reece Jones and Wolfgang Warneke <reece@>
2016-06-01 11:35:55 -0700

    add angry version element which converts letter to all uppercase (is slightly buggy)

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 
 $(document).ready(function() {
+  var lettercopy;
   $("#nameget form").submit(function(event) {
     var yourname = $("input#yourname").val();
     console.log(yourname);
-    var letter = "<h3>Dear " + yourname + ",</h3><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor, " + yourname + ", incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><h3>Yours truly,</h3><h2><em id=\"signature\">Your Secret Admirer</em>&nbsp;<span class=\"glyphicon glyphicon-heart\" aria-hidden=\"true\"></span></h2>"
+    var letter = "<h3>Dear " + yourname + ",</h3><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor, " + yourname + ", incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><h3>Yours truly,</h3><h2><em id=\"signature\">Your Secret Admirer</em>&nbsp;<span class=\"glyphicon glyphicon-heart\" aria-hidden=\"true\"></span></h2>";
+    lettercopy = letter;
 
     $("#yourletter").empty();
     $("#yourletter").append(letter);
@@ -13,5 +15,11 @@ $(document).ready(function() {
 
   $(".btn-danger").click(function() {
     $("#yourletter").hide();
+  });
+
+  $(".btn-warning").click(function() {
+    $("#yourletter").empty();
+    var angryletter = lettercopy.toUpperCase();
+    $("#yourletter").append(angryletter);
   });
 });
